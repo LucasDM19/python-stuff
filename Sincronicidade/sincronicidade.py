02e1db5c86b2b953dfabc796e73e5f16264cf906
Lucas <lucasmuzel@gmail.com>
2018-08-07 16:58:51 -0300

    Sincronicidade
    Novo item - Sincronicidade em Python

---
+++ Sincronicidade/sincronicidade.py
@@ -0,0 +1,9 @@
+﻿#coding=utf-8
+"""
+ Experimento do dia 23/12/2015
+ A pedido do Ronaldo, 'For science!', executarei uma vez o código abaixo, 
+ que tem como objetivo gerar 10000 números binários, entre 0 e 1. 
+ Deixarei a entrada salva, durante alguns meses.
+"""
+import random
+print [random.randint(0, 1) for i in range(10000)]
